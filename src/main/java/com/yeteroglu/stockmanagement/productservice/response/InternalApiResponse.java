ff368dd5a6633f183410a203f0a218a1d3807159
BerkYeteroglu <berkyeterogluth@gmail.com>
2024-01-18 16:25:35 +0300

    Created InternalApiResponse

--- src/main/java/com/yeteroglu/stockmanagement/productservice/response/InternalApiResponse.java
+++ src/main/java/com/yeteroglu/stockmanagement/productservice/response/InternalApiResponse.java
@@ -2,8 +2,16 @@ package com.yeteroglu.stockmanagement.productservice.response;
 
 import lombok.Builder;
 import lombok.Data;
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
 
 @Data
 @Builder
-public class InternalApiResponse {
+public class InternalApiResponse<T> {
+    private FriendlyMessage friendlyMessage;
+    private T payload;
+    private boolean hasError;
+    private List<String> errorMessage;
+    private HttpStatus httpStatus;
 }
